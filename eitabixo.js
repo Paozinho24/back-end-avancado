8f30fe0736d57d933169966404ca936bae3bc02e
Kaua <k.g.a2014@outlook.com>
2022-06-07 18:18:50 -0500

    teste

---
+++ eitabixo.js
@@ -0,0 +1,69 @@
+console.log("Hello MOTO")
+
+
+//Declarando variaveis HAHAHAHAHHA
+    var x = 10 //Essa variável é do tipo Number
+//Formas mais convenientes de declarar 
+let y = 24 
+
+const z = ''
+
+const t = z + 10 
+
+
+y = 20 
+console.log(y)
+
+if(true){
+    let y = 15
+    console.log(y)
+
+}
+
+console.log(y)
+
+//tipos de cornos
+
+
+const nome = "KAUA PINTO GRANDE"
+console.log(nome)
+console.log(typeof nome)
+
+const age = 20
+console.log(age)
+console.log(typeof age)
+
+const peso = 72.3
+console.log(peso)
+console.log(typeof peso)
+
+//tipo null
+
+const nada = null
+console.log(nada)
+console.log(typeof nada)
+
+//tipo undefined
+
+let thiago 
+console.log(thiago)
+console.log(typeof thiago)
+
+//tipo boll
+
+const vouf = true
+console.log(vouf)
+console.log(typeof vouf)
+
+//Array
+
+const eitabixovetor = ['java', 'C', 'Comi tua mae', 'Kevin é gay']
+console.log(eitabixovetor)
+console.log(typeof eitabixovetor)
+
+console.log(eitabixovetor.length)
+
+
+
+
+

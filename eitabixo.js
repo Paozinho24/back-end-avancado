0104f9cb40a84e72bd211c2670bfc3f4c100f530
Kaua <k.g.a2014@outlook.com>
2022-06-07 19:16:17 -0500

    ignore a minha zueira

--- eitabixo.js
+++ eitabixo.js
@@ -25,7 +25,7 @@ console.log(y)
 //tipos de cornos
 
 
-const nome = "KAUA PINTO GRANDE"
+const nome = "kevin gay"
 console.log(nome)
 console.log(typeof nome)
 
@@ -64,6 +64,71 @@ console.log(typeof eitabixovetor)
 console.log(eitabixovetor.length)
 
 
+console.log("--------------------------------------------------------------------")
 
 
+//object literals
 
+const usuario = {
+    email: 'vontadeDeMorrer@gmail.com' ,
+    senha: 'morrer123' ,
+    nome: 'Renatinho Emo' ,
+    idade: 31
+}
+
+console.log(usuario.email)
+
+console.log(typeof usuario)
+
+console.log("--------------------------------------------------------------------")
+
+const Teste = 'eita bixo'
+console.log(Teste.length)
+const stringParavetor = Teste.split(' ')
+console.log(stringParavetor)
+
+console.log("--------------------------------------------------------------------")
+console.log(Teste.toUpperCase())
+console.log(Teste.toLowerCase())
+nomeMaiusculo = Teste.toUpperCase()
+console.log(nomeMaiusculo)
+
+console.log(nomeMaiusculo.indexOf('BI'))
+
+console.log("-----------------------------------------------------------------------")
+console.log(nomeMaiusculo[nomeMaiusculo.length - 1])
+console.log(nomeMaiusculo.slice(0 , 4))
+
+console.log("-----------------------------------------------------------------------")
+
+//array, vetores, listas
+
+const lista = [ 'a', 'b','c','d','e']
+console.log(lista.length)
+console.log(typeof lista)
+console.log(lista[2])
+lista[5] = 'foda né, linguagem dianamica filhao'
+console.log(lista[5])
+lista[0] = 'x'
+console.log(lista)
+lista.push('g')
+console.log(lista)
+lista.pop()
+console.log(lista)
+lista.shift()
+console.log(lista)
+lista.unshift('h')
+console.log(lista)
+
+
+//object literals - estruturas que descrevem entidades
+
+const produto = {
+    nome : "VIBRADOR",
+    preco: 250.0 ,
+    estoque: true ,
+    size: ['70 cm ' , '120cm' , '30cm ' , '10cm']
+}
+
+produto.nome
+produto['nome']
